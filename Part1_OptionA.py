1d166cddcb7d9163681b662cf7a241b98b030e42
Ng Chun Ho <166355312+ktmc-210079@users.noreply.github.com>
2025-06-30 12:53:49 +0800

    Add files via upload

---
+++ Part1_OptionA.py
@@ -0,0 +1,20 @@
+import Part1_PaymentGenerator as gen
+from Part1_ParameterValues import *
+
+avg = 0
+
+for i in gen.gen(T):
+    #for each possibility
+    balance = W
+    #reset value
+    for j in range(T):
+        #for each payment
+        balance += balance * R
+        #interest
+        balance -= (int(i[-T + j]) + 1) * 5
+        #payment
+    avg += balance
+
+avg = avg / 2 ** T
+
+print(avg)
