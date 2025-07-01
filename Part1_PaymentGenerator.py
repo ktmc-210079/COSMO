de9b3e41a13ab5ff6d686941211175589e46d4e1
Ng Chun Ho <166355312+ktmc-210079@users.noreply.github.com>
2025-07-01 17:16:22 +0800

    Update Part1_PaymentGenerator.py

--- Part1_PaymentGenerator.py
+++ Part1_PaymentGenerator.py
@@ -1,5 +1,3 @@
-#generate a list of binary numbers in the format '0b10000', where the digits after the '1' indicate 5 BTC (0) or 10 BTC (10)
-
 def gen(n):
     x = []
     y = 2**n
@@ -7,6 +5,7 @@ def gen(n):
         x.append(bin(y))
         y += 1
     return x
-
+#generate a list of binary numbers in the format '0b10000', where the digits after the '1' indicate 5 BTC (0) or 10 BTC (10)
 if __name__ == "__main__":
     print(gen(int(input())))
+    input()
